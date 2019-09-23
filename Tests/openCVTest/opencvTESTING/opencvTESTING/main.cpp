0d983f3ddd5f554cb7a96b4d90c08acfa6c35c9d
Abhiram Santhosh <abhisanthosh@cmail.carleton.ca>
2019-09-23 12:43:12 -0400

    test commit

---
+++ Tests/openCVTest/opencvTESTING/opencvTESTING/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencvTESTING
+//
+//  Created by Abhiram Santhosh on 9/23/19.
+//  Copyright © 2019 Abhi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
